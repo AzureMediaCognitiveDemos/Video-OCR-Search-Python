ba0f1d83d37df864e429a3f51056a974ca371b3c
Yoichi Kawasaki <yoichika@microsoft.com>
2016-10-26 23:08:45 +0000

    added new demo contents

---
+++ demo/azuresubs/video/DOWNLOAD.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget http://yoichikademo.blob.core.windows.net/amscogs/TransferanAzuresubscriptionJP.mp4
---
+++ demo/decoderinna/video/DOWNLOAD.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget http://yoichikademo.blob.core.windows.net/amscogs/decode2016-rinna-DBP019.mp4
